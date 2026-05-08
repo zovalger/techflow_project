2c495b715ebbbeb3b9f6c907c84e979e387eb204
zovalger <zovalger@gmail.com>
2026-05-08 01:40:22 -0400

    modelo

--- addons/tech_inventory/models/tech_rating.py
+++ addons/tech_inventory/models/tech_rating.py
@@ -8,15 +8,63 @@ class TechRating(models.Model):
 
     equipment_id = fields.Many2one(
         'tech.equipment', 
-        string='Equipo',
-        required=True
+        string = 'Equipo',
+        required = True
     )
 
     evaluated_by_id = fields.Many2one(
         'res.users', 
-        string='Evaluador',
-        default= lambda self: self.env.user,
-        required=True
+        string ='Evaluador',
+        default = lambda self: self.env.user,
+        required = True
     )
     
-    rating_date = fields.Date(string='Fecha de Compra')
+    rating_date = fields.Date(
+        string = 'Fecha de la valoración',
+        required = True,
+        default = fields.Date.today
+        )
+    
+    rating = fields.Selection([
+        ('bad', 'Malo'),
+        ('regular', 'Regular'),
+        ('good', 'Excelente')
+        ], 
+        string = 'Valoración',
+        required = True,
+        default='available',
+    )      
+
+    is_recommended = fields.Boolean(
+        string = 'Es recomendado',
+        store=True,
+        compute="_compute_is_recommended",
+    )
+    
+    active = fields.Boolean(
+        string = 'Activo',
+        required = True,
+        default = False
+    )
+
+    name = fields.Char(
+        string='Nombre de la valoración',
+        store=True,
+        compute="_compute_name",
+    )
+
+    @api.depends('rating') 
+    def _compute_is_recommended(self): 
+        for record in self:
+            if record.rating == 'good':   
+                record.is_recommended = True
+            else:
+                record.is_recommended = False
+
+    @api.depends('rating_date', 'equipment_id') 
+    def _compute_name(self): 
+        for record in self:
+            if record.equipment_id.name and record.rating_date:
+                record.name = f"Valoración: {record.equipment_id.name} ({record.rating_date})"
+            else:
+                record.name = f"Nueva Valoración: ({record.rating_date})"
